916c9c38712f60e77bf79e1505bf01c60734a89b
Atul72 <rai703537@gmail.com>
2024-09-25 07:55:58 +0530

    implemented free email service unlike resend

---
+++ lib/email.ts
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer";
+
+//from: "onboarding@resend.dev",
+// to: email,
+// subject: "2FA Code",
+// html: `<p>Your 2FA code: ${token}</p>`,
+
+interface optionsInterface {
+  to: string;
+  subject: string;
+  html: string;
+}
+
+export const sendEmail = async (options: optionsInterface) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: {
+      name: "auth-email",
+      address: process.env.USER,
+    },
+    to: options.to,
+    subject: options.subject,
+    html: options.html,
+  };
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
--- lib/mail.ts
+++ lib/mail.ts
@@ -1,36 +1,59 @@
 import { Resend } from "resend";
+import { sendEmail } from "@/lib/email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  // await resend.emails.send({
+  //   from: "onboarding@resend.dev",
+  //   to: email,
+  //   subject: "2FA Code",
+  //   html: `<p>Your 2FA code: ${token}</p>`,
+  // });
+
+  // Khud ka free of cost email sending machine
+  const options = {
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
-  });
+  };
+  await sendEmail(options);
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  // await resend.emails.send({
+  //   from: "onboarding@resend.dev",
+  //   to: email,
+  //   subject: "Reset your password",
+  //   html: `<p>Click <a href=${resetLink}>here</a> to confirm email.</p>`,
+  // });
+
+  const options = {
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href=${resetLink}>here</a> to confirm email.</p>`,
-  });
+  };
+  await sendEmail(options);
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  // await resend.emails.send({
+  //   from: "onboarding@resend.dev",
+  //   to: email,
+  //   subject: "Confirm your email",
+  //   html: `<p>Click <a href=${confirmLink}>here</a> to confirm email.</p>`,
+  // });
+
+  const options = {
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href=${confirmLink}>here</a> to confirm email.</p>`,
-  });
+  };
+  await sendEmail(options);
 };
